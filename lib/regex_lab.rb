d464be73ca127066f5b0d1d9743424b2ce7e733c
Cory Marker <cmarker4@gmail.com>
2020-07-23 02:28:13 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,7 +25,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if text.match(/^[A-Z].)  && text.match(/[\.[$/)
+  if text.match(/^[A-Z].)  && text.match(/[\.]$/)
     true
   else
     false
